d13a97bbc9b47fd101f07b36e3e7888e7cd218c4
Marc Grimpo <mgrimpo.dev@gmail.com>
2022-09-08 01:56:07 +0200

    refactor data flow

--- download_text_pdf.py
+++ download_text_pdf.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import dataclasses
 from numbers import Number
 import requests
 import os
@@ -6,55 +7,72 @@ from PyPDF2 import PdfMerger, PdfReader
 
 from get_pdf_links import EpisodeLink, get_episode_links
 
-DOWNLOAD_FOLDER = 'downloads'
+DOWNLOAD_FOLDER = "downloads"
+
+
+@dataclass
+class EpisodePdf:
+    episode_number: int
+    path: str
+    num_pages: int | None  # None represents 'number of pages unknown'
+
 
 def main():
     episode_links = get_episode_links()
 
     os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
-    for episode_link in episode_links:
-        download_episode_pdf(episode_link)
+    episode_pdfs_without_page_meta_data = map(download_episode_pdf, episode_links)
 
-    episode_pdfs = map(read_pdf_meta_data, episode_links)
+    episode_pdfs = map(read_pdf_meta_data, episode_pdfs_without_page_meta_data)
     merge_pdfs(episode_pdfs)
 
-@dataclass
-class EpisodePdf:
-    episode_number: int
-    num_pages: int
 
 def merge_pdfs(episode_pdfs: list[EpisodePdf]):
     pdfMerger = PdfMerger()
     current_page = 0
     for episode_pdf in episode_pdfs:
-        pdfMerger.append(_out_pdf_path(episode_pdf.episode_number))
-        pdfMerger.add_outline_item(pagenum=current_page, title=f'Episode {episode_pdf.episode_number}')
+        pdfMerger.append(episode_pdf.path)
+        pdfMerger.add_outline_item(
+            pagenum=current_page, title=f"Episode {episode_pdf.episode_number}"
+        )
         current_page += episode_pdf.num_pages
     pdfMerger.write("merged.pdf")
-    
 
-def read_pdf_meta_data(episode_link: EpisodeLink) -> list[EpisodePdf]:
-    pdfReader = PdfReader(_download_pdf_path(episode_link.episode_number))
-    return EpisodePdf(int(episode_link.episode_number), len(pdfReader.pages))
-    
 
-def download_episode_pdf(episode_link: EpisodeLink):
-    if os.path.exists(_download_pdf_path(episode_link.episode_number)):
-        response_size = requests.head(episode_link.url).headers.get('content-length', -1)
+def read_pdf_meta_data(episode_pdf: EpisodePdf) -> list[EpisodePdf]:
+    pdfReader = PdfReader(_download_pdf_path(episode_pdf.episode_number))
+    num_pages = len(pdfReader.pages)
+    return dataclasses.replace(episode_pdf, num_pages=num_pages)
+
+
+def download_episode_pdf(episode_link: EpisodeLink) -> EpisodePdf:
+    """Download pdfs for the given links, writes them to disk and returns EpisodePdf metatdata"""
+    episode_pdf = EpisodePdf(
+        episode_number=episode_link.episode_number,
+        path=_download_pdf_path(episode_link.episode_number),
+        num_pages=None,  # unknown as of yet
+    )
+    if os.path.exists(episode_pdf.path):
+        response_size = requests.head(episode_link.url).headers.get(
+            "content-length", -1
+        )
         response_size = int(response_size)
-        if response_size > 0 and response_size == os.path.getsize(_download_pdf_path(episode_link.episode_number)):
-            print(f'Skipping download of episode {episode_link.episode_number} PDF as local file exists with same size as remote file ({response_size} bytes)')
-            return
-    with open(_download_pdf_path(episode_link.episode_number), 'wb') as f:
+        if response_size > 0 and response_size == os.path.getsize(episode_pdf.path):
+            _log_skip(episode_link, response_size)
+            return episode_pdf
+
+    with open(episode_pdf.path) as f:
         f.write(requests.get(episode_link.url).content)
+    return episode_pdf
+
+
+def _log_skip(episode_link, response_size):
+    print(
+        f"Skipping download of episode {episode_link.episode_number} PDF as local file exists with same size as remote file ({response_size} bytes)"
+    )
 
 def _download_pdf_path(episode_number: Number):
-     return f'{DOWNLOAD_FOLDER}{os.sep}{episode_number}.pdf'
-
-def _out_pdf_path(episode_number: Number):
-     return f'out{os.sep}{episode_number}.pdf'
-
-
+    return f"{DOWNLOAD_FOLDER}{os.sep}{episode_number}.pdf"
 
 
 if __name__ == "__main__":
