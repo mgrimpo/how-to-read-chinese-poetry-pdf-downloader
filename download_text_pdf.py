d28a9af84738bd66d21fdb014c78be10a8d0f67a
Marc Grimpo <mgrimpo.dev@gmail.com>
2022-09-08 01:22:16 +0200

    add working script

--- download_text_pdf.py
+++ download_text_pdf.py
@@ -1,5 +1,61 @@
-def main():
-    pass
+from dataclasses import dataclass
+from numbers import Number
+import requests
+import os
+from PyPDF2 import PdfMerger, PdfReader
 
-if __name__ == '__main__':
+from get_pdf_links import EpisodeLink, get_episode_links
+
+DOWNLOAD_FOLDER = 'downloads'
+
+def main():
+    episode_links = get_episode_links()
+
+    os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+    for episode_link in episode_links:
+        download_episode_pdf(episode_link)
+
+    episode_pdfs = map(read_pdf_meta_data, episode_links)
+    merge_pdfs(episode_pdfs)
+
+@dataclass
+class EpisodePdf:
+    episode_number: int
+    num_pages: int
+
+def merge_pdfs(episode_pdfs: list[EpisodePdf]):
+    pdfMerger = PdfMerger()
+    current_page = 0
+    for episode_pdf in episode_pdfs:
+        pdfMerger.append(_out_pdf_path(episode_pdf.episode_number))
+        pdfMerger.add_outline_item(pagenum=current_page, title=f'Episode {episode_pdf.episode_number}')
+        current_page += episode_pdf.num_pages
+    pdfMerger.write("merged.pdf")
+    
+
+def read_pdf_meta_data(episode_link: EpisodeLink) -> list[EpisodePdf]:
+    pdfReader = PdfReader(_download_pdf_path(episode_link.episode_number))
+    return EpisodePdf(int(episode_link.episode_number), len(pdfReader.pages))
+    
+
+def download_episode_pdf(episode_link: EpisodeLink):
+    if os.path.exists(_download_pdf_path(episode_link.episode_number)):
+        response_size = requests.head(episode_link.url).headers.get('content-length', -1)
+        response_size = int(response_size)
+        if response_size > 0 and response_size == os.path.getsize(_download_pdf_path(episode_link.episode_number)):
+            print(f'Skipping download of episode {episode_link.episode_number} PDF as local file exists with same size as remote file ({response_size} bytes)')
+            return
+    with open(_download_pdf_path(episode_link.episode_number), 'wb') as f:
+        f.write(requests.get(episode_link.url).content)
+
+def _download_pdf_path(episode_number: Number):
+     return f'{DOWNLOAD_FOLDER}{os.sep}{episode_number}.pdf'
+
+def _out_pdf_path(episode_number: Number):
+     return f'out{os.sep}{episode_number}.pdf'
+
+
+
+
+if __name__ == "__main__":
     main()
---
+++ get_pdf_links.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+import re
+import requests
+from dataclasses import dataclass
+from bs4 import BeautifulSoup, Tag
+
+HTRCP_PODCAST_PAGE_URL = "https://howtoreadchinesepoetry.com/"
+
+@dataclass
+class EpisodeLink:
+    url: str
+    episode_number: str
+
+def get_episode_links():
+    podcast_page_html = requests.get(HTRCP_PODCAST_PAGE_URL).text
+    soup = BeautifulSoup(podcast_page_html, "html.parser")
+    strong_elements = soup.find_all("strong")
+    episode_labels = filter(content_starts_with_episode, strong_elements)
+    episode_links : list[EpisodeLink] = [] 
+    for episode_label in episode_labels:
+        match find_link_in_siblings(episode_label):
+            case str(link):
+                regex = re.compile("Episode (\d+)")
+                episode_number = regex.search(episode_label.text.strip()).group(1)
+                episode_links.append(EpisodeLink(link, episode_number))
+            case unknown:
+                raise Exception("Unknown result ", unknown)
+    print(*episode_links, sep="\n")
+    return episode_links
+    
+
+
+def find_link_in_siblings(elem: Tag) -> Tag:
+    anchor_element = elem.find_next_sibling('a')
+    match anchor_element:
+        case None: return None
+        case Tag(attrs = {"href": href, **rest}):
+            return href
+        case _ :
+            raise Exception(anchor_element, " was unexpected")
+
+
+
+def content_starts_with_episode(elem: Tag) -> bool:
+   """ Check for elements with string content starting with 'Episode'
+   """
+
+   match elem:
+        case None:
+            return False
+        case Tag(contents = contents):
+            if not isinstance(contents[0], str):
+                return False
+            return contents[0].strip().startswith("Episode")
+        case unexpected_value:
+            raise Exception("Didn't expect: ", unexpected_value)
